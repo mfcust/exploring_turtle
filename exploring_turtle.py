a85bbd33b607f799edb644f3daa91c7c22e0e356
mfcust <43455391+mfcust@users.noreply.github.com>
2020-09-18 08:42:25 -0700

    Update exploring_turtle.py

--- exploring_turtle.py
+++ exploring_turtle.py
@@ -55,16 +55,64 @@ t.end_fill()
 
 
 #YOU CAN EVEN CHANGE YOUR TURTLE'S SHAPE! Use the .shape('type shape in here') function to set the turtle's shape. Your options are: Square, Arrow, Circle, Turtle,
-Triangle, Classic
+#Triangle, Classic
 
 
 
+
+#Guess what else? You could have made most of these alterations to your turtle in one line of code
+#e.g. t.pen(pencolor="yellow", fillcolor="blue", pensize=10, speed=1) ->Try it below!!
 
 
 
 #Bonus Questions:
 
 
-1)The eval() function takes in strings, converts them to integers or floats and then calculates the value of whatever the string was. For example
+# 1)Review Question: There are three new functions that can help you with this bonus math problem. str() converts whatever is inside it's parentheses to a string data type.
+#The float() function converts whatever is in the parentheses into a float, and the int() function converts whatever is in the parentheses into an integer.
+#e.g. print(str(4.57893)) would print the string '4.57893'
+
+#You are helping your friend order some pizza. Your friend is looking at the menu and telling you how many pizzas they want, as well as the cost of the pizza. The
+#pizza has a sales tax of 8%(.08). Follow the prompts in order to determine how much your order will cost (Hint: You may need to use the input function as well!):
+
+
+#a) First ask your friend how many pizzas they want to order and store that as variable num_pizzas
+
+
+#b)Then ask your friend how much each pizza costs and store that as a variable pizza_cost
+
+
+#c)Calculate the total cost of your pizzas before tax and store that as variable subtotal_pizza
+
+
+#d)Calculate the sales tax owed on your order and store that as variable sales_tax
+tax_rate = 0.08
+
+
+#e)Add the sales tax to your order subtotal and store that as variable total
+
+
+#f) Tell your friend how much the entire order will cost using the variable total in your print statement (make sure you write out a message and use + to add
+#strings together in your print statement
+
+
+
+
+#2)Take a potential answer to the last assignment's spiral bonus question:
+t.reset()
+colors = ["red", "yellow", "blue", "green"] 
+for x in range(100):
+  t.pencolor(colors[x%4]) t.forward(x)
+  t.left(91)
+  
+#Instead of drawing a coninuous line, could you add steps in here to make it a dashed line? What if you want to write your name over and over again in a spiral-like
+#pattern? What if you wanted your name to increase in size each time you wrote it? Hint: Look up the write() function for turtle and see what parameters it takes.
+#See if you can incorporate it into your program!
+
+
+
+
+
+
 
 
